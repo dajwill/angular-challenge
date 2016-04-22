8d9add3f1be1fffbec7e9bb5e77d41ae2ce20a06
dajwilli <david.williams@webascender.com>
2016-04-21 23:00:23 -0400

    completed challenge

---
+++ app.js
@@ -0,0 +1,43 @@
+app = angular.module('fibApp', []);
+app.controller('fibCtrl', function($scope) {
+    $scope.fib = 0;
+    $scope.number = 0;
+    $scope.type = 'iterative';
+
+    function fib(x) {
+      if ($scope.type == 'recursive'){
+        if(x < 1) {
+          return 0;
+        }
+        else if (x == 1) {
+          return 1;
+        } else {
+          return fib(x - 1) + fib(x - 2);
+        }
+      } else {
+        var array = [0, 1];
+        if(x > 1){
+          for(var i = 2; i < x + 1; i++){
+            array.push(array[array.length - 1] + array[array.length - 2])
+          }
+        }
+        return array[x]
+      }
+    };
+
+    $scope.isIterative = function() {
+      if($scope.type == 'iterative') {
+        return true
+      }
+    }
+
+    $scope.nextNumber = function() {
+      $scope.number = $scope.number + 1;
+      $scope.fib = fib($scope.number);
+    }
+
+    $scope.reset = function(){
+      $scope.fib = 0
+      $scope.number = 0
+    }
+});
